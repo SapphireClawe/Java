629e3875b65f97259e06c708f09e0b216775751f
SapphireClawe <37085710+SapphireClawe@users.noreply.github.com>
2018-09-21 13:43:04 -0600

    Files

---
+++ Swapping Variables within objects/index.js
@@ -0,0 +1,27 @@
+console.log('Hello World');
+
+// Create 10 variables about yourself
+let SarahFlesher = {
+  firstname: "Sarah",
+  lastname: "Flesher",
+  age: "18",
+  ageAfraid: null,
+  height: "5'7",
+  hair colour: "blonde",
+  eye colour: "blue",
+  beauty: null,
+  female: true,
+}
+
+
+// Print variables to console
+console.log(name);
+console.log(age);
+console.log(ageAfraid);
+console.log(height);
+console.log(hair colour);
+console.log(eye colour);
+console.log(beauty);
+console.log(female);
+
+console.log(SarahFlesher);
