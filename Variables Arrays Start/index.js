909b55001b5bca6614b1ddaa6614002b86206109
SapphireClawe <37085710+SapphireClawe@users.noreply.github.com>
2018-09-24 12:54:10 -0600

    Array

---
+++ Variables Arrays Start/index.js
@@ -0,0 +1,29 @@
+console.log('Swapping Variables within objects');
+
+// Create 10 variables about yourself
+let SarahFlesher = {
+  firstname: "Sarah",
+  lastname: "Flesher",
+  age: "18",
+  ageAfraid: null,
+  height: "5'7",
+  hair colour: "blonde",
+  eye colour: "blue",
+  beauty: null,
+  female: true,
+}
+
+
+// Print variables to console
+console.log(name);
+console.log(age);
+console.log(ageAfraid);
+console.log(height);
+console.log(hair colour);
+console.log(eye colour);
+console.log(beauty);
+console.log(female);
+
+console.log(SarahFlesher);
+
+console.log(SarahFlesher.firstname);
