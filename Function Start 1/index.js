75fd9567d17fd510dc93c997484af8f2619c8b3f
SapphireClawe <37085710+SapphireClawe@users.noreply.github.com>
2018-09-24 13:33:20 -0600

    Hello

---
+++ Function Start 1/index.js
@@ -0,0 +1,19 @@
+// Create 10 variables about yourself
+
+
+let SarahFlesher = [];
+let MeganSpence = [];
+
+// firstname, lastname, age, ageAfraid, hair, female, male
+
+SarahFlesher = ["Sarah", "Flesher", 18, null, true, true, false];
+MeganSpence = ["Megan", "Spence", 14, null, true, true, false];
+
+
+
+
+console.log("Last name of SarahFlesher is: " = SarahFlesher[1]);
+console.log("Last name of MeganSpence is: " = MeganSpence[1]);
+
+SarahFlesher[1]="Is Awesome";
+console.log(SarahFlesher[1]);
