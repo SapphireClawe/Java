489a399ab66d4c46fd03414d8b1d2b14e23f27c7
SapphireClawe <37085710+SapphireClawe@users.noreply.github.com>
2018-09-24 13:30:32 -0600

    Etc

--- Variables Arrays Start/index.js
+++ Variables Arrays Start/index.js
@@ -1,4 +1,6 @@
 // Create 10 variables about yourself
+
+
 let SarahFlesher = [];
 let MeganSpence = [];
 
@@ -10,13 +12,8 @@ MeganSpence = ["Megan", "Spence", 14, null, true, true, false];
 
 
 
-// Print variables to console
-console.log(SarahFlesher[]);
-console.log(MeganSpence[]);
-
-
-console.log("Last name is: " = SarahFlesher[1]);
-console.log("Last name is: " = MeganSpence[1]);
+console.log("Last name of SarahFlesher is: " = SarahFlesher[1]);
+console.log("Last name of MeganSpence is: " = MeganSpence[1]);
 
 SarahFlesher[1]="Is Awesome";
 console.log(SarahFlesher[1]);
